afc4377db47584d9f0bd97141426aeabd97c8ae8
Anyerson <lorddeep3@gmail.com>
2023-07-11 14:10:11 -0500

    Three Commit

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,4 +1,31 @@
 import "../style/div.scss"
 
-console.log('Prueba 77');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuOverlay = document.querySelector('.menu-overlay');
+    const menuClose = document.querySelector('.menu-close');
+ 
+  
+    // Función para mostrar el menú desplegable
+    function showMenu() {
+      menuOverlay.classList.remove('hidden');
+    }
+  
+    // Función para ocultar el menú desplegable
+    function hideMenu() {
+      menuOverlay.classList.add('hidden');
+    }
+  
+    // Evento click en el icono de menú
+    menuIcon.addEventListener('click', function () {
+      showMenu();
+    });
+  
+    // Evento click en el icono de cierre
+    menuClose.addEventListener('click', function () {
+      hideMenu();
+    });
+  
+  });
+  
 
